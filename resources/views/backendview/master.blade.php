541381e370bcc574f307e7b7c7481e34b672e296
jobayer-alam-24 <sheikhnayamul123@gmail.com>
2024-08-28 02:24:31 +0600

    updated

---
+++ resources/views/backendview/master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+
+    @include('backendview.includes.header')
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  @include('backendview.includes.sidebar')
+
+  <div class="content-wrapper">
+    @yield('content')
+  </div> 
+
+
+  @include('backendview.includes.footer')
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+@include('backendview.includes.js')
+</body>
+</html>
